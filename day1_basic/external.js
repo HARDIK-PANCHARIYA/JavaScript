7dda9c3810835b2aa529529f53a3a8d68f049237
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-24 10:06:04 +0530

    JavaScript Basic

---
+++ day1_basic/external.js
@@ -0,0 +1,8 @@
+// WAP to print area of triangle  ---- 1/2 * b * h
+
+var base = parseInt(prompt("Enter Base"))
+var height=parseInt(prompt("Enter Height"))
+
+var aot= 0.5*base*height
+
+document.write("Area of Triangle ="+aot)
