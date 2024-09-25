28fd76ea12313267798c6d6daad3850706069516
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-25 14:22:40 +0530

    JavaScript statement and operator

---
+++ statement_and_operator/statement_and_operato.js
@@ -0,0 +1,27 @@
+// -------------------------- conditional statement ----------------------------------
+// 1. selection statement - if. if/else, nested if/else
+// 2. Looping statement - for, while, do while loops 
+// 3. Branching statement - switch case 
+
+
+// --------------------- operators -------------------------------------------
+// 1. increment/decrement ---- ++ , --  (a++, ++a, a--, --a)
+// 2. aritmatematic ---- +, -, *, /, %
+// 3. logical --- &&, ||, !
+// 4. relational --- <, >, >=, <=, !=, ==
+// 5. assigment ---- +=. -=, *=, /=, = 
+// 6. conditional or ternary --- ?, :
+
+
+
+// WAP to print number is even or odd
+
+var num=parseInt(prompt("Enter any Number = "))
+
+if(num%2==0){
+    console.log("Number is Even")
+
+}
+else{
+    console.log("Number is Odd")
+}
