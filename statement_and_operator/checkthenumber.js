a8c9f009dd6a5d33d8b17a3cddabf424a30edc1d
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-26 10:08:52 +0530

    JavaScript if else

---
+++ statement_and_operator/checkthenumber.js
@@ -0,0 +1,9 @@
+// WAP to cheak given number is divisible by 3 And 5
+
+var num = parseInt(prompt("Enter Number = "));
+
+if(num%3==0 && num%5==0){
+    document.write("Number is divisible by 3 and 5 ")
+}else{
+    document.write("Number is not divisible by 3 and 5 ")
+}
