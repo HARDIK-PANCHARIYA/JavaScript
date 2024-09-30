24f852623ebdfa569ab14f78cd06b63c83968602
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-09-30 09:10:06 +0530

    for loop

---
+++ looping/looping.js
@@ -0,0 +1,8 @@
+// ---------------- looping in javascript ------------------
+
+// for loop, while loop, do while
+// forEach, for of, for in, map, filter
+
+for(var i=1; i<10; i++){
+    console.log("Welcome Hardik")
+}
